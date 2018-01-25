2f78a42f9add8fdd7ca4332c219401ff3f3d6613
Otari Kobiashvili <otarikobi@gmail.com>
2018-01-25 18:02:56 -0500

    Add Coinmarketcap API for use

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,46 @@
 import _ from 'lodash';
 
-const ROOT_URL = "https://min-api.cryptocompare.com/";
+const ROOT_URL = "https://api.coinmarketcap.com/v1/ticker/?";
 
 export const GET_COINS = "GET_ALL_COINS";
 
 export function fetchAllCoins() {
   return function(dispatch) {
-    fetch(`${ROOT_URL}data/all/coinlist`)
+    fetch(`${ROOT_URL}start=0&limit=100`)
     .then( coins => {
-      return coins = coins.json();
+      return coins=coins.json();
     })
     .then( coins => {
-      return coins = _.mapKeys(coins.Data, (value, key) => {
-        return value.SortOrder;
-      })
-    })
-    .then( coins => {
-      return coins = Object.values(coins).slice(0, 100);
-    })
-    .then( coins =>  {
       const action = {
         type: GET_COINS,
         payload: coins
       }
-      dispatch(action)
+
+      dispatch(action);
     })
   }
 }
+
+// export function fetchAllCoins() {
+//   return function(dispatch) {
+//     fetch(`${ROOT_URL}data/all/coinlist`)
+//     .then( coins => {
+//       return coins = coins.json();
+//     })
+//     .then( coins => {
+//       return coins = _.mapKeys(coins.Data, (value, key) => {
+//         return value.SortOrder;
+//       })
+//     })
+//     .then( coins => {
+//       return coins = Object.values(coins).slice(0, 100);
+//     })
+//     .then( coins =>  {
+//       const action = {
+//         type: GET_COINS,
+//         payload: coins
+//       }
+//       dispatch(action)
+//     })
+//   }
+// }
--- src/reducers/reducer_AllCoins.js
+++ src/reducers/reducer_AllCoins.js
@@ -3,7 +3,7 @@ import { GET_COINS } from "../actions/index";
 export default function(state={}, action) {
   switch (action.type) {
     case GET_COINS:
-      console.log(action.payload)
+      console.log(action.payload);
       return action.payload;
     default:
       return state;
